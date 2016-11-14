f311e89f1d9301499e082275103e320606f2b0d4
Charlotte Wilson <charlotteawilson.94@gmail.com>
2016-11-14 07:30:12 +0000

    constants

---
+++ Constants.py
@@ -0,0 +1,18 @@
+"""
+Created on 14 November 2016
+@author: Charlotte Alexandra Wilson
+
+Last revision: 14 Nov 2016 
+
+Simply a constants file for variables used throughout the program
+"""
+
+class Constants():
+	TARGET = "Hello World!"
+	CROSSOVER_RATE = 7;
+	MUTATION_CHANCE = 0.1;
+	MAX_GEN = 3000;
+	POP_SIZE = 100;
+
+# Still need to add a crossover size etc.
+
